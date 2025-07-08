960fb4b6577b25d3bf44e16f23f769af1d5c7544
Nitansh <24b1807@iitb.ac.in>
2025-07-08 18:04:16 +0530

    added navbar

---
+++ spandan-front-end/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+// import { Link } from 'react-router-dom'
+import './Navbar.css'
+
+function Navbar() {
+  return (
+    <nav id="navbar">
+        <img alt="biox logo" src=""></img>
+        <ul id="navitems">
+            <li>Home</li>
+            <li>Team</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
